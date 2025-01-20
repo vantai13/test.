96fe284ad27ec2bc265c812fa98449390975124b
vantai13 <taidoan.13072005@gmail.com>
2025-01-20 09:19:54 +0700

    first-commit

---
+++ vantai.py
@@ -0,0 +1,13 @@
+print("hello world")
+print("Chào các bạn ")
+
+vantai=input("doan van tai ")   
+
+print(vantai)
+
+number = int(vantai)
+
+# print(type(number))
+# print(number)
+# print(number)
+print("vantai")
